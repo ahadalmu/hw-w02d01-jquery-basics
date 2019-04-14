d080789b287fbc180bcbb1d78d02314c03fefbc9
ahad mu <ahadamash@gmail.com>
2019-04-15 01:39:29 +0300

    done ;)

--- index.js
+++ index.js
@@ -2,19 +2,68 @@
 function turnOnStopLight() {
     // change background color of stop light to red
     // changes background color of green and yellow to black
+var on = $('.bulb').css('background-color')
+   $('#stopLight').css('background-color','red');
+   $('#slowLight').css('background-color','black');
+   $('#goLight').css('background-color','black');
+
+   //  turnOffLights();
 }
+
+$('#stopButton').on('click', turnOnStopLight);
 
 // turn on slow function
 function turnOnSlowLight() { 
   // change background color of slow light to yellow
   // changes background color of red and green to black
+  var on = $('.bulb').css('background-color')
+  $('#stopLight').css('background-color','black');
+  $('#slowLight').css('background-color','yellow');
+  $('#goLight').css('background-color','black');  
+
+  // turnOffLights();
+
 }
+
+$('#slowButton').on('click', turnOnSlowLight);
+
+
 
 // turn on go function
 function turnOnGoLight() {
     // change background color of go light to green
     // changes background color of red and yellow to black
-}
+    var on = $('.bulb').css('background-color')
+   $('#stopLight').css('background-color','black');
+   $('#slowLight').css('background-color','black');
+   $('#goLight').css('background-color','green');
+   
+   // turnOffLights();
+
+  }
+  
+  $('#goButton').on('click', turnOnGoLight);
+  
+
+
+
+// function turnOffLights(){
+
+//   if (this === turnOnStopLight){
+//   $('#stopLight').css('background-color','black');
+//   $('#slowLight','#goLight').attr('background-color','black');
+// }
+// else if(this === turnOnSlowLight){
+//   $('#stopLight').css('background-color','black');
+//   $('#slowLight').css('background-color','black');
+//   $('#goLight').css('background-color','black');
+// }
+// else (this === turnOnGoLight)
+//   $('#stopLight').css('background-color','black');
+//   $('#slowLight').css('background-color','black');
+//   $('#goLight').css('background-color','black');
+// }
+
 
 // set stop light click event
 
